648b43058e4ecdd70bd1037d992bbd02f310e8ce
abhinavhans <abhinavhans1997@gmail.com>
2023-05-20 22:47:43 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,111 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-black.png';
+
+import shaq from './personal/shaq.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png';
+import rubyrails from './tech/rubyrails.png';
+import java from './tech/java.png';
+import sql from './tech/sql.jpg';
+import whimsical from './tech/whimsical.jpg';
+
+import coverhunt from './company/coverhunt.png';
+import dcc from './company/dcc.png';
+import kelhel from './company/kelhel.png';
+import microverse from './company/microverse.png';
+import nutanix from './company/nutanix.png';
+import hsbc from './company/hsbc.png';
+import nextleap from './company/nextleap.jpg';
+import bangalore from './company/bangalore.jpg';
+
+import komikult from './projects/komikult.png';
+import leaderboard from './projects/leaderboard.png';
+import math from './projects/math-magicians.png';
+import movie from './projects/movie-metro.png';
+import nyeusi from './projects/nyeusi.png';
+import space from './projects/space-hub.png';
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  shaq,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  figma,
+  whimsical,
+  git,
+  sql,
+  graphql,
+  html,
+  javascript,
+  nodejs,
+  postgresql,
+  rubyrails,
+  reactjs,
+  java,
+  redux,
+  tailwind,
+  typescript,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+  nutanix,
+  hsbc,
+  nextleap,
+  bangalore,
+};
